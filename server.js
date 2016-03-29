7019abe080b272e1d2718bd55200ffaab6d49906
Kenneth Lim <kennethlimcp@gmail.com>
2016-03-29 12:57:48 +0800

    add: POST route
    - tested with REST client
    - html submission is not working yet
    
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
 
+// instruct the app to use the `bodyParser()` middleware for all routes
+app.use(bodyParser());
 app.use(express.static(__dirname));
 
+app.post('/', function(req, res){
+	res.send(req.body);
+	console.log("received a POST!");
+});
+
 var server = app.listen(8080, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('App is listening at http://%s:%s', host, port)
+	var host = server.address().address
+	var port = server.address().port
+	console.log('App is listening at http://%s:%s', host, port)
 })
